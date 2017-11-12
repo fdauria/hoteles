47a68ce39274840662a06fcdd4ea669a66c8b43a
fdauria <fdauria@tekgenesis.com>
2017-11-12 18:41:05 -0300

    OfertaJMS + logs

--- TPO_Grupo12_EJB/ejbModule/integracion/Props.java
+++ TPO_Grupo12_EJB/ejbModule/integracion/Props.java
@@ -5,18 +5,22 @@ public class Props {
 	public Props(){
 		
 	}
-	
-	public static String connection_factory_value = "jms/RemoteConnectionFactory";
-	public static String ofertaToJMS_PROVIDER_URL_value = "http-remoting://192.168.0.101:8080";
-
-	public static String usernameOfertaToJMS = "hornetq";
-
-	public static String passwordOfertaToJMS = "hornetq";
-	public static String destinationOfertaToJMS = "jms/queue/ofertasHotel";
-	public static String usernameConnOferta = "hornetq";
-	public static String passwordConnOferta = "hornetq";
-	public static String URL_SEND_HOTEL_BACKOFFICE 	= "http://192.168.0.107:8080/TPO_BO_WEB/rest/ServiciosBO/EnviarSolicitud";
-	public static String URL_TO_LOG 				= "http://192.168.0.107:8080/TPO_BO_WEB/rest/ServiciosBO/RegistrarLog";
+	public static String IP_PORTAL_WEB 					= "192.168.0.101:8080";
+	public static String connection_factory_value 		= "jms/RemoteConnectionFactory";
+	public static String ofertaToJMS_PROVIDER_URL_value = "http-remoting://" + IP_PORTAL_WEB;
+	public static String destinationOfertaToJMS 		= "jms/queue/ofertasHotel";
+	public static String usernameOfertaToJMS 			= "hornetq";
+	public static String passwordOfertaToJMS 			= "hornetq";
+	public static String usernameConnOferta  			= "hornetq";
+	public static String passwordConnOferta  			= "hornetq";
 	
 	public static String IP_LOCAL = "192.168.0.104";
+	
+	public static String IP_BACKOFFICE 						= "http://192.168.0.108:8080";
+	public static String URL_OBTENER_SERVICIOS 				= IP_BACKOFFICE + "/TPO_BO_WEB/rest/ServiciosBO/GetServicios";
+	public static String URL_OBTENER_SERVICIOS_POR_TIPO 	= IP_BACKOFFICE + "/TPO_BO_WEB/rest/ServiciosBO/GetServiciosPorTipo";
+	public static String URL_SEND_HOTEL_BACKOFFICE 			= IP_BACKOFFICE + "/TPO_BO_WEB/rest/ServiciosBO/EnviarSolicitud";
+	public static String URL_TO_LOG 						= IP_BACKOFFICE + "/TPO_BO_WEB/rest/ServiciosBO/RegistrarLog";
+	
+
 }
